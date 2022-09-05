c954cc855970863274bdb0f3e7281bef1deb8d9d
John Weachock <jweachock@squareup.com>
2022-09-04 21:42:17 -0400

    [Base] Fix default ADDRESS
    Uhg

--- src/Base.js
+++ src/Base.js
@@ -1,6 +1,6 @@
 import { LobbyClient } from 'boardgame.io/client';
 
-export const ADDRESS = process.env.ADDRESS || "http://localhost:5000";
+export const ADDRESS = process.env.ADDRESS || "https://xtfc-solstice.herokuapp.com";
 export const lobbyClient = new LobbyClient({ server: ADDRESS });
 
 console.log("ADDRESS: " + ADDRESS);
