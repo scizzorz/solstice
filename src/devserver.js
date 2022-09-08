fa811a967a71df029771cd5545431bf7f7d253c7
John Weachock <jweachock@gmail.com>
2022-09-08 11:09:00 -0400

    [server] Add human-readable-ids for match IDs

--- src/devserver.js
+++ src/devserver.js
@@ -2,6 +2,7 @@ import path from 'path';
 import serve from 'koa-static';
 import { Server, Origins } from 'boardgame.io/server';
 import { Solstice } from './Game';
+import { getID } from 'human-readable-ids-browser';
 
 const ADDRESS = process.env.ADDRESS || "https://xtfc-solstice.herokuapp.com/";
 const PORT = process.env.PORT || 8000;
@@ -12,6 +13,7 @@ console.log("PORT: " + PORT);
 const server = Server({
   games: [Solstice],
   origins: [Origins.LOCALHOST, ADDRESS],
+  uuid: getID,
 });
 
 server.run(PORT);
